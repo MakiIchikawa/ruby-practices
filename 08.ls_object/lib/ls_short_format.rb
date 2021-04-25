a8173a4cfc4c40b79dcb5aaa14ccb97cd14c3bfc
MakiIchikawa <punkfolkfolk@gmail.com>
2021-04-25 17:30:45 +0900

    calc_number_of_rowメソッドの不要なコードを削除

--- 08.ls_object/lib/ls_short_format.rb
+++ 08.ls_object/lib/ls_short_format.rb
@@ -20,11 +20,10 @@ class LsShortFormat < Ls
   end
 
   def calc_number_of_row(files_number, max_column_number = 3, default_row_number = 4)
-    custom_row_number = (files_number.to_f / max_column_number).ceil
     if files_number <= max_column_number * default_row_number
       default_row_number
     else
-      custom_row_number
+      (files_number.to_f / max_column_number).ceil
     end
   end
 
