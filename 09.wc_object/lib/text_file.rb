982eb88458956406486da18bb6d976e879dd7a3c
MakiIchikawa <punkfolkfolk@gmail.com>
2021-04-26 17:06:32 +0900

    TextFileクラスにメソッドを追加

--- 09.wc_object/lib/text_file.rb
+++ 09.wc_object/lib/text_file.rb
@@ -11,4 +11,18 @@ class TextFile
   def add_line(line)
     lines << line
   end
+
+  def number_of_lines
+    lines.length
+  end
+
+  def number_of_words
+    lines.sum do |line|
+      line.split(/[\s,\n]/).delete_if(&:empty?).length
+    end
+  end
+
+  def bytes
+    lines.sum(&:bytesize)
+  end
 end
--- 09.wc_object/test/text_file_test.rb
+++ 09.wc_object/test/text_file_test.rb
@@ -4,11 +4,26 @@ require 'minitest/autorun'
 require_relative '../lib/text_file'
 
 class TestTextFile < Minitest::Test
+  def setup
+    @textfile = TextFile.new('test.txt')
+    @textfile.add_line("apple\n")
+    @textfile.add_line("orange\n")
+    @textfile.add_line("grape pine\n")
+  end
+
   def test_add_lines
-    textfile = TextFile.new('test.txt')
-    textfile.add_line("apple\n")
-    textfile.add_line("orange\n")
-    textfile.add_line("grape\n")
-    assert_equal %W[apple\n orange\n grape\n], textfile.lines
+    assert_equal ["apple\n", "orange\n", "grape pine\n"], @textfile.lines
+  end
+
+  def test_number_of_lines
+    assert_equal 3, @textfile.number_of_lines
+  end
+
+  def test_number_of_words
+    assert_equal 4, @textfile.number_of_words
+  end
+
+  def test_bytes
+    assert_equal 24, @textfile.bytes
   end
 end
