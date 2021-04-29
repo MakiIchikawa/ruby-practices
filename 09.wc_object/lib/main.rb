f0496a9a890d60fabaf013563fc1c639bf312b4a
MakiIchikawa <punkfolkfolk@gmail.com>
2021-04-29 09:06:12 +0900

    メインルーチンの不備を修正

--- 09.wc_object/lib/main.rb
+++ 09.wc_object/lib/main.rb
@@ -17,7 +17,6 @@ text_file = first_filename ? TextFile.new(first_filename) : TextFile.new('-')
 files.add_file(text_file)
 ARGF.each do |line|
   unless files.exist?(ARGF.filename)
-    files.add_file(text_file)
     text_file = TextFile.new(ARGF.filename)
     files.add_file(text_file)
   end
