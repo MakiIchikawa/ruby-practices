d378d4f9c170a8b56b947df300ebde25f29f2d5a
MakiIchikawa <punkfolkfolk@gmail.com>
2021-05-04 18:09:00 +0900

    Wcクラスの仕様変更に伴い、メインルーチンを修正

--- 09.wc_object/lib/main.rb
+++ 09.wc_object/lib/main.rb
@@ -11,21 +11,23 @@ l_option = false
 opt.on('-l') { |v| l_option = v }
 opt.parse!(ARGV)
 
-wc = Wc.new
+files = []
 text_file = TextFile.new(ARGF.filename)
-wc.add_file(text_file)
+files << text_file
 ARGF.each do |line|
-  unless wc.add_files_names.include?(ARGF.filename)
+  unless files.map(&:file_name).include?(ARGF.filename)
     text_file = TextFile.new(ARGF.filename)
-    wc.add_file(text_file)
+    files << text_file
   end
   text_file.add_line(line)
 end
+
+wc = Wc.new(files)
 
 counts = []
 counts << wc.number_of_rows
 counts << wc.number_of_words << wc.bytes unless l_option
 
-wc_out = WcOut.new(counts, wc.add_files_names)
+wc_out = WcOut.new(counts, wc.files_names)
 
 puts wc_out.output
